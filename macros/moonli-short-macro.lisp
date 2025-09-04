34713ee2776698eb00d57ac21e9aac0f53347f96
Shubhamkar Ayare <digikar@proton.me>
2025-09-04 21:41:26 +0200

    [add] short if macro

--- macros/moonli-short-macro.lisp
+++ macros/moonli-short-macro.lisp
@@ -19,8 +19,20 @@
   ((name symbol))
   `(in-package ,name))
 
+(define-moonli-short-macro if
+  ((test moonli-expression)
+   (_ +whitespace/internal)
+   (then moonli-expression)
+   (_ (esrap:? +whitespace/internal))
+   (else (esrap:? moonli-expression)))
+  `(if ,test ,then ,else))
+
 (5am:def-test short-macro-call ()
   (5am:is (equal `(defvar a 5)
                  (esrap:parse 'short-macro-call "defvar a 5")))
   (5am:is (equal `(defparameter a 5)
-                 (esrap:parse 'short-macro-call "defparameter a 5"))))
+                 (esrap:parse 'short-macro-call "defparameter a 5")))
+  (5am:is (equal `(if a 5 nil)
+                 (esrap:parse 'short-macro-call "if a 5")))
+  (5am:is (equal `(if a :hello :bye)
+                 (esrap:parse 'short-macro-call "if a :hello :bye"))))
