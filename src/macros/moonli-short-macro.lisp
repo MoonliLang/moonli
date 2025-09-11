b03730dd1e6231d591d4624e5c500287a5446b9a
Shubhamkar Ayare <digikar@proton.me>
2025-09-11 08:14:59 +0200

    [change] use an = sign in defvar and defparameter

--- src/macros/moonli-short-macro.lisp
+++ src/macros/moonli-short-macro.lisp
@@ -6,11 +6,15 @@
 (define-moonli-short-macro defvar
   ((name good-symbol)
    (_ +whitespace/internal)
+   (_ "=")
+   (_ +whitespace/internal)
    (value moonli-expression))
   `(defvar ,name ,value))
 
 (define-moonli-short-macro defparameter
   ((name good-symbol)
+   (_ +whitespace/internal)
+   (_ "=")
    (_ +whitespace/internal)
    (value moonli-expression))
   `(defparameter ,name ,value))
@@ -41,9 +45,9 @@
 
 (5am:def-test short-macro-call ()
   (5am:is (equal `(defvar a 5)
-                 (esrap:parse 'short-macro-call "defvar a 5")))
+                 (esrap:parse 'short-macro-call "defvar a = 5")))
   (5am:is (equal `(defparameter a 5)
-                 (esrap:parse 'short-macro-call "defparameter a 5")))
+                 (esrap:parse 'short-macro-call "defparameter a = 5")))
   (5am:is (equal `(if a 5 nil)
                  (esrap:parse 'short-macro-call "ifelse a 5")))
   (5am:is (equal `(if a :hello :bye)
