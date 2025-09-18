5df60c776a1caf1c6b6242b6fb7abf57f2de86d7
Shubhamkar Ayare <digikar@proton.me>
2025-09-18 08:18:45 +0200

    [fix] use cl-repl:*line-continue-function*

--- repl/main.lisp
+++ repl/main.lisp
@@ -4,3 +4,6 @@
 (in-package :moonli-repl)
 
 (setf cl-repl:*read-function* 'moonli:read-moonli-from-string)
+(setf cl-repl:*line-continue-function*
+      (lambda (string)
+        (nth-value 1 (ignore-errors (moonli:read-moonli-from-string string)))))
